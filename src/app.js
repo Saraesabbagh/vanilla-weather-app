42f5daa09c9910028e0a89bf5d840fd1bad885d8
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2021-11-07 16:38:33 +0000

    removed console logs

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#city-name");
   let descriptionElement = document.querySelector("#weather-description");
@@ -41,5 +40,5 @@ function displayTemperature(response) {
 let apiKey = "68bf029e8355926f228cd5965fe12dec";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
