26fbbdeac0a8a9913a8b11fc7da37ee48eea8b34
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2021-11-08 20:05:25 +0000

    added coordinates, and new forecasting api for upcoming days

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tues", "Wed", "Thu"];
@@ -41,6 +42,13 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "68bf029e8355926f228cd5965fe12dec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -65,6 +73,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -108,5 +118,3 @@ flink.addEventListener("click", displayFahrenheitTemp);
 
 let clink = document.querySelector("#c-link");
 clink.addEventListener("click", displaycelsiusTemp);
-
-displayForecast();
